9623dbdba99587e0244215e7736dee2100d092e9
Romina Ghanaati <rominaghanaati@gmail.com>
2023-09-29 08:22:05 +0330

    new commit 29 Sep

---
+++ js/end.js
@@ -0,0 +1,25 @@
+const score = JSON.parse(localStorage.getItem("score"));
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const scoreEle = document.querySelector("h1");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+scoreEle.innerText = score;
+
+const saveHandler = () => {
+    const userName = input.value;
+    if(!userName || !score) {
+        alert("invalid username or score");
+    } else {
+        const finalScore = {name: userName, score}
+        highScores.push(finalScore);
+        highScores.sort((a,b) => {b.score - a.score});
+        highScores.splice(10);
+
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        localStorage.removeItem("scores");
+        window.location.assign("/")
+}}
+
+button.addEventListener("click", saveHandler);
